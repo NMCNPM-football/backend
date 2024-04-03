046b235938554e0461f74042b50a83a983f0877f
phat072 <phanphatlqd2003@gmail.com>
2024-04-04 02:07:43 +0700

    add firebase

--- cmd/firebase/push_mysql_test.go
+++ cmd/firebase/push_mysql_test.go
@@ -1,10 +1,13 @@
 package firebase
 
 import (
+	"cloud.google.com/go/firestore"
+	"context"
 	"database/sql"
 	"fmt"
 	"github.com/NMCNPM-football/backend/cmd/firebase/mysql"
 	"github.com/NMCNPM-football/backend/internal/models"
+	"google.golang.org/api/option"
 	"log"
 	"testing"
 )
@@ -12,13 +15,18 @@ import (
 func TestPushData(t *testing.T) {
 	log.Println("Testing Push Data")
 	list := pushMySQLData()
-
+	firestoreClient := createClient(context.TODO())
+	for _, v := range list {
+		addDataToFireStore(context.TODO(), firestoreClient, v)
+	}
 	if len(list) > 0 {
-		log.Println(fmt.Sprint("#v", list[0]))
+		log.Println(fmt.Sprint("#v", list))
 		log.Println("Data is pushed successfully")
 	} else {
 		log.Println("Data is not pushed successfully")
 	}
+
+	_ = firestoreClient
 }
 
 func pushMySQLData() []models.User {
@@ -65,4 +73,38 @@ func pushMySQLData() []models.User {
 	}
 	list = append(list, newUserData)
 	return list
+}
+
+func createClient(ctx context.Context) *firestore.Client {
+	projectID := "se104-71786"
+	client, err := firestore.NewClient(ctx, projectID, option.WithCredentialsFile("./firebase1.json"))
+
+	if err != nil {
+		log.Fatalf("Fail to create client: %v", err)
+	}
+	return client
+}
+
+func addDataToFireStore(ctx context.Context, client *firestore.Client, user models.User) {
+	_, _, err := client.Collection("users").Add(ctx, map[string]interface{}{
+		"ID":              user.ID,
+		"CreatedAt":       user.CreatedAt,
+		"UpdatedAt":       user.UpdatedAt,
+		"DeletedAt":       user.DeletedAt,
+		"Name":            user.Name,
+		"Email":           user.Email,
+		"Phone":           user.Phone,
+		"Club":            user.Club,
+		"Address":         user.Address,
+		"Password":        user.Password,
+		"ClubID":          user.ClubID,
+		"Position":        user.Position,
+		"IsVerifiedEmail": user.IsVerifiedEmail,
+		"SeaSon":          user.SeaSon,
+		"UpdatedBy":       user.UpdatedBy,
+		"DeletedBy":       user.DeletedBy,
+	})
+	if err != nil {
+		log.Fatalf("Fail to add data: %v", err)
+	}
 }
