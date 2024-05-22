20f0ec54830923ad3081564a3489ae5a3f7033bb
phat072 <phanphatlqd2003@gmail.com>
2024-05-22 15:49:35 +0700

    fix space

--- database /cardtype.sql
+++
@@ -1,6 +0,0 @@
-USE SE104;
-
-INSERT INTO `card_types` (card_type_id, card_type_name) VALUES
-                                                            ('LT01','Thẻ vàng'),
-                                                            ('LT02','Thẻ đỏ');
-
